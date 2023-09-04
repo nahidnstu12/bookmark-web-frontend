1bca58c19b76d0eb81e913ce6a941d0e19d644a6
sajeda.akhter704@gmail.com <sajedasaju85@gmail.com>
2023-09-04 18:26:07 +0600

    select field done

---
+++ @core/components/Inputs/CustomCheckbox.tsx
@@ -0,0 +1,13 @@
+type Props = {
+  id: string;
+  label: string | number | React.ReactNode;
+  isLoading?: boolean;
+  register?: any;
+  errorInstance?: any;
+  checked?: boolean | number;
+  onChange?: (event?: React.ChangeEvent<HTMLInputElement>) => void;
+  value?: any;
+};
+
+const CustomCheckbox = () => {};
+export default CustomCheckbox;
